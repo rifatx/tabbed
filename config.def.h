9ed51d40abedc8b590e0bff382fda2a645886870
rifatx <rifat.aricanli@gmail.com>
2024-02-21 18:22:55 +0300

    autohide tabbar

--- config.def.h
+++ config.def.h
@@ -33,7 +33,7 @@ static Bool npisrelative  = False;
         } \
 }
 
-#define MODKEY ControlMask
+#define MODKEY ControlMask|Mod1Mask
 static const Key keys[] = {
 	/* modifier             key        function     argument */
 	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
@@ -63,4 +63,14 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
 
 	{ 0,                    XK_F11,    fullscreen,  { 0 } },
+  { MODKEY,               XK_Shift_L, showbar,    { .i = 1 } },
+  { ShiftMask,            XK_Control_L, showbar,    { .i = 1 } },
+ };
+
+static Key keyreleases[] = {
+	/* modifier             key          function     argument */
+//	{ 0,                    XK_Shift_L,  NULL,   { 0 } },
+  { MODKEY|ShiftMask,     XK_Shift_L,  showbar,     { .i = 0 } },
+  { MODKEY|ShiftMask,     XK_Control_L,  showbar,     { .i = 0 } },
+ 
 };
